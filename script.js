9041cd6fab4cab524e321827a23811419aa581f6
IBRAHIM102005 <ibrahimmmdov5@gmail.com>
2023-09-16 15:57:19 +0400

    function

---
+++ script.js
@@ -0,0 +1,75 @@
+// let n = prompt();
+// function bolme(n) {
+//     if (n % 7 == 0) {
+//         return "7-e bolunur";
+//     }
+//     else {
+//         a = n / 7;
+//         b = Math.round(a);
+//         return b * 7;
+//     }
+
+
+// } console.log(bolme(n));
+
+
+// let n = prompt();
+// s = 0
+// function say(n) {
+//     if (n < 50) {
+//         for (let i = 1; i < n; i++) {
+//             if (i % 3 == 0) {
+//                 s = s + 1;
+
+//             }
+
+//         }
+//         return s;
+
+//     }
+
+// }
+// console.log(say(n));
+
+
+
+
+
+
+// let n = prompt();
+// s = 0
+// function say(n) {
+//     if (n > 50 && n < 100) {
+//         for (let i = 1; i < n; i++) {
+//             if (i % 5 == 0) {
+//                 s = s + 1;
+
+//             }
+
+//         }
+//         return s;
+
+//     }
+
+// }
+// console.log(say(n));
+
+
+
+let n = prompt();
+s = 0
+function say(n) {
+    if (n > 100) {
+        for (let i = 1; i < n; i++) {
+            if (i % 8 == 0) {
+                s = s + 1;
+
+            }
+
+        }
+        return s;
+
+    }
+
+}
+console.log(say(n));
